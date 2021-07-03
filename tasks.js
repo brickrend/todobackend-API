9cf46a12eae22f197bbc0db6ef36f12dc16f6301
brickrend <Brickrends9@gmail.com>
2021-07-03 16:35:30 +0300

    work in progress

--- tasks.js
+++ tasks.js
@@ -3,13 +3,13 @@ const tasks = [
     id: 1,
     name: "Clean the kitchen",
     description: "description",
-    done: true,
+    status: true,
   },
   {
     id: 2,
     name: "Iron clothes",
     description: "description",
-    done: false,
+    status: false,
   },
 ];
 module.exports = tasks;
